67bb505bf465a9c49dd098f4d2137da51e707593
Rickard Lindberg <rickard@rickardlindberg.me>
2024-12-20 21:37:50 +0100

    Day 11 part 2.

--- 11.py
+++ 11.py
@@ -1,4 +1,6 @@
 """
+Examples:
+
 >>> Stones.load_text("125 17").evolve(1)
 253000 1 7
 
@@ -8,10 +10,15 @@
 >>> Stones.load_text("125 17").evolve(6)
 2097446912 14168 4048 2 0 2 4 40 48 2024 40 48 80 96 2 8 6 7 6 0 3 2
 
+Part 1:
+
 >>> Stones.load_text("510613 358 84 40702 4373582 2 0 1584").evolve(25).count()
 217812
 
-#>>> Stones.load_text("510613 358 84 40702 4373582 2 0 1584").evolve(75).count()
+Part 2:
+
+>>> Stones.load_text("510613 358 84 40702 4373582 2 0 1584").evolve(75).count()
+259112729857522
 """
 
 import doctest
@@ -26,24 +33,17 @@ class Stones:
             stones.add(Stone(int(number)))
         return stones
 
-    def __init__(self):
-        self.stones = []
+    def __init__(self, stones=[]):
+        self.stones = list(stones)
 
     def add(self, stone):
         self.stones.append(stone)
 
-    def evolve(self, number):
-        if number == 0:
-            return self
-        else:
-            stones = Stones()
-            for stone in self.stones:
-                for new in stone.evolve():
-                    stones.add(new)
-            return stones.evolve(number - 1)
+    def evolve(self, times):
+        return Stones([stone.evolve(times) for stone in self.stones])
 
     def count(self):
-        return len(self.stones)
+        return sum(stone.count() for stone in self.stones)
 
     def __repr__(self):
         return " ".join(repr(x) for x in self.stones)
@@ -54,21 +54,48 @@ class Stone:
     def __init__(self, number):
         self.number = number
 
-    def evolve(self):
-        result = []
-        if self.number == 0:
-            result.append(Stone(1))
-        elif len(str(self.number)) % 2 == 0:
-            text = str(self.number)
-            split_index = len(text) // 2
-            result.append(Stone(int(text[:split_index])))
-            result.append(Stone(int(text[split_index:])))
-        else:
-            result.append(Stone(self.number * 2024))
-        return result
+    def evolve(self, times):
+        return LazyStone(number=self.number, times=times)
+
+    def count(self):
+        return 1
 
     def __repr__(self):
         return str(self.number)
+
+
+class LazyStone:
+
+    cache = {}
+
+    def __init__(self, number, times):
+        self.number = number
+        self.times = times
+
+    def evolve(self, times):
+        if times == 0:
+            return Stone(self.number)
+        else:
+            stones = Stones()
+            if self.number == 0:
+                stones.add(Stone(1))
+            elif len(str(self.number)) % 2 == 0:
+                text = str(self.number)
+                split_index = len(text) // 2
+                stones.add(Stone(int(text[:split_index])))
+                stones.add(Stone(int(text[split_index:])))
+            else:
+                stones.add(Stone(self.number * 2024))
+            return stones.evolve(times - 1)
+
+    def count(self):
+        key = (self.number, self.times)
+        if key not in self.cache:
+            self.cache[key] = self.evolve(self.times).count()
+        return self.cache[key]
+
+    def __repr__(self):
+        return repr(self.evolve(self.times))
 
 
 doctest.testmod()
