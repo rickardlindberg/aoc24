4a344851a6028741a977082f70cdb0d6f050e795
Rickard Lindberg <rickard@rickardlindberg.me>
2024-12-25 19:30:33 +0100

    Add cache per design in attempt to get different answer for part 2.

--- 19.py
+++ 19.py
@@ -1,4 +1,14 @@
 """
+Examples:
+
+>>> onsen = OnsenParser().parse_lines([
+...     "r, g, rg",
+...     "",
+...     "rg",
+... ])
+>>> onsen.count_ways_to_make_towel_designs()
+2
+
 >>> onsen = OnsenParser().parse_lines([
 ...     "r, wr, b, g, bwu, rb, gb, br",
 ...     "",
@@ -17,12 +27,23 @@
 8
 >>> onsen.count_possible_towel_designs()
 6
+>>> onsen.count_ways_to_make_towel_designs()
+16
 
 Part 1:
 
->>> OnsenParser().parse().count_possible_towel_designs()
+>>> onsen = OnsenParser().parse()
+>>> onsen.count_possible_towel_designs()
 367
+
+Part 2:
+
+>>> onsen.count_ways_to_make_towel_designs()
+718579937078294   too low
+2807139376154360  too high
 """
+
+import pprint
 
 class OnsenParser:
 
@@ -37,7 +58,7 @@ class OnsenParser:
                 for towel in line.split(", "):
                     onsen.add_towel(Towel(towel))
             elif index > 1:
-                onsen.add_design(line.strip())
+                onsen.add_design(Design(line.strip()))
         return onsen
 
 class Towel:
@@ -58,16 +79,36 @@ class Towel:
     def __repr__(self):
         return f"Towel({self.towel!r})"
 
+class Design:
+
+    def __init__(self, design):
+        self.design = design
+        self.cache = {}
+
+    def count_ways_to_make(self, towels):
+        return self.inner(self.design, towels)
+
+    def inner(self, design, towels):
+        if design not in self.cache:
+            if design:
+                self.cache[design] = 0
+                for towel in towels:
+                    try:
+                        self.cache[design] += self.inner(towel.design(design), towels)
+                    except DesignNotPossible:
+                        pass
+            else:
+                self.cache[design] = 1
+        return self.cache[design]
+
 class Onsen:
 
     def __init__(self):
         self.towels = []
         self.designs = []
-        self.cache = {}
 
     def add_towel(self, towel):
         self.towels.append(towel)
-        self.cache = {}
 
     def add_design(self, design):
         self.designs.append(design)
@@ -75,26 +116,15 @@ class Onsen:
     def count_possible_towel_designs(self):
         count = 0
         for design in self.designs:
-            if self.is_possible(design):
+            if design.count_ways_to_make(self.towels) > 0:
                 count += 1
         return count
 
-    def is_possible(self, design):
-        if design not in self.cache:
-            self.cache[design] = self.calculate_is_possible(design)
-        return self.cache[design]
-
-    def calculate_is_possible(self, design):
-        if design:
-            for towel in self.towels:
-                try:
-                    if self.is_possible(towel.design(design)):
-                        return True
-                except DesignNotPossible:
-                    pass
-            return False
-        else:
-            return True
+    def count_ways_to_make_towel_designs(self):
+        return sum(
+            design.count_ways_to_make(self.towels)
+            for design in self.designs
+        )
 
 class DesignNotPossible(Exception):
     pass
