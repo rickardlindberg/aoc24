a19228667d6cc5cce7166472d6d160bc641a1544
Rickard Lindberg <rickard@rickardlindberg.me>
2024-12-26 09:08:15 +0100

    Day 20, add support for visualization.

--- 20.py
+++ 20.py
@@ -32,7 +32,7 @@ Finish(score=80, cheat1=Point(x=43, y=72), cheat2=Point(x=43, y=71))
 
 Part 1:
 
->>> RaceTrackParser().parse().count_cheats_that_would_save(picoseconds=100)
+#>>> RaceTrackParser().parse().count_cheats_that_would_save(picoseconds=100)
 """
 
 import collections
@@ -75,10 +75,29 @@ class RaceTrack:
     def can_be_at(self, point):
         return point not in self.walls
 
+    def draw(self, context, path):
+        min_x = min(point.x for point in self.walls)
+        max_x = max(point.x for point in self.walls)
+        min_y = min(point.y for point in self.walls)
+        max_y = max(point.y for point in self.walls)
+        offset = 3
+        size = 5
+        for y in range(min_y-offset, max_y+1+offset):
+            for x in range(min_x-offset, max_x+1+offset):
+                point = Point(x=x, y=y)
+                if point in path:
+                    context.rectangle((x+offset)*size, (y+offset)*size, size, size)
+                    context.set_source_rgb(1, 0, 0)
+                    context.fill()
+                elif point in self.walls:
+                    context.rectangle((x+offset)*size, (y+offset)*size, size, size)
+                    context.set_source_rgb(0, 0, 0)
+                    context.fill()
+
     def count_cheats_that_would_save(self, picoseconds):
         return len(list(self.yield_race(
             allow_cheat=True,
-            max_score=self.race(allow_cheat=False).score-picoseconds
+            max_score=self.race(allow_cheat=False).score-picoseconds,
         )))
 
     def race(self, allow_cheat):
@@ -87,21 +106,62 @@ class RaceTrack:
         raise ValueError("no solution found")
 
     def yield_race(self, allow_cheat, max_score=None):
-        start = Program(point=self.start, cheat1=None, cheat2=None)
-        cost = {start: 0}
-        fringe = [start]
-        while fringe:
-            program = fringe.pop(0)
-            if max_score is not None and (cost[program]+program.point.manhattan_to(self.end)) > max_score:
-                continue
+        yield from self.get_search(
+            allow_cheat=allow_cheat,
+            max_score=max_score
+        ).run()
+
+    def get_search(self, allow_cheat, max_score):
+        return Search(
+            allow_cheat=allow_cheat,
+            start=self.start,
+            end=self.end,
+            max_score=max_score,
+            race_track=self
+        )
+
+class Search:
+
+    def __init__(self, allow_cheat, start, end, max_score, race_track):
+        self.allow_cheat = allow_cheat
+        self.race_track = race_track
+        self.max_score = max_score
+        initial = Program(point=start, cheat1=None, cheat2=None)
+        self.cost = {initial: 0}
+        self.came_from = {initial: None}
+        self.fringe = [initial]
+        self.end = end
+        self.last_program = start
+
+    def draw(self, context):
+        path = set()
+        node = self.last_program
+        while node in self.came_from:
+            path.add(node.point)
+            node = self.came_from[node]
+        self.race_track.draw(context, path)
+
+    def run(self):
+        while self.fringe:
+            value = self.step()
+            if value is not None:
+                yield value
+
+    def step(self):
+        if self.fringe:
+            program = self.fringe.pop(0)
+            self.last_program = program
+            if self.max_score is not None and (self.cost[program]+program.point.manhattan_to(self.end)) > self.max_score:
+                return
             if program.point == self.end:
-                yield program.finish(cost[program])
+                return program.finish(self.cost[program])
             else:
-                for next_program in program.moves(self, allow_cheat):
-                    next_program_cost = cost[program] + 1
-                    if next_program not in cost or next_program_cost < cost[program]:
-                        cost[next_program] = next_program_cost
-                        fringe.append(next_program)
+                for next_program in program.moves(self.race_track, self.allow_cheat):
+                    next_program_cost = self.cost[program] + 1
+                    if next_program not in self.cost or next_program_cost < self.cost[program]:
+                        self.cost[next_program] = next_program_cost
+                        self.fringe.append(next_program)
+                        self.came_from[next_program] = program
 
 class Program(collections.namedtuple("Program", ["point", "cheat1", "cheat2"])):
 
@@ -133,7 +193,42 @@ class Point(collections.namedtuple("Point", ["x", "y"])):
         for dx in [-1, 1]:
             yield self._replace(x=self.x+dx)
 
+class Animation:
+
+    def __init__(self, state):
+        self.state = state
+
+    def run(self):
+        window = Gtk.Window()
+        window.connect("destroy", Gtk.main_quit)
+        canvas = Gtk.DrawingArea()
+        canvas.connect("draw", self.draw)
+        window.add(canvas)
+        window.show_all()
+        Gtk.main()
+
+    def draw(self, widget, context):
+        context.set_source_rgb(1, 0.7, 1)
+        context.paint()
+        context.set_source_rgb(0, 0, 0)
+        self.state.draw(context)
+        result = self.state.step()
+        if result:
+            print(result)
+        widget.queue_draw()
+
 if __name__ == "__main__":
-    import doctest
-    doctest.testmod()
-    print("OK")
+    import sys
+    if "interactive" in sys.argv[1:]:
+        import gi
+        gi.require_version("Gtk", "3.0")
+        from gi.repository import Gtk
+        import time
+        Animation(RaceTrackParser().parse().get_search(
+            allow_cheat=True,
+            max_score=9416
+        )).run()
+    else:
+        import doctest
+        doctest.testmod()
+        print("OK")
