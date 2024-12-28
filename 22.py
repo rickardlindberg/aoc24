c8aad50ada90cadc97d25ed26cdbbeff6af1985b
Rickard Lindberg <rickard@rickardlindberg.me>
2024-12-28 20:39:18 +0100

    Can get sequences of length 4 and their prices.

--- 22.py
+++ 22.py
@@ -1,10 +1,31 @@
 """
+Examples:
+
+>>> secret_numbers = SecretNumbers()
+>>> secret_numbers.add(SecretNumber(1))
+>>> secret_numbers.add(SecretNumber(10))
+>>> secret_numbers.add(SecretNumber(100))
+>>> secret_numbers.add(SecretNumber(2024))
+>>> secret_numbers.sum_2000()
+37327623
+
+>>> secret_numbers = SecretNumbers()
+>>> secret_numbers.add(SecretNumber(1))
+>>> secret_numbers.add(SecretNumber(2))
+>>> secret_numbers.add(SecretNumber(3))
+>>> secret_numbers.add(SecretNumber(2024))
+>>> secret_numbers.sum_2000()
+37990510
+
 Part 1:
 
 >>> NumberParser().parse().sum_2000()
 16999668565
+
+Part 2:
 """
 
+import collections
 import operator
 
 class NumberParser:
@@ -41,12 +62,39 @@ class SecretNumber:
 
     def simulate_2000(self):
         """
-        >>> SecretNumber(1).simulate_2000()
+        >>> SecretNumber(1).simulate_2000().as_integer()
         8685429
         """
         for secret_number in self.simulate(2000):
             pass
         return secret_number
+
+    def sequences(self, iterations=2000):
+        """
+        >>> for x in SecretNumber(123).simulate(9):
+        ...     print(x)
+        15887950: 0 (-3)
+        16495136: 6 (6)
+        527345: 5 (-1)
+        704524: 4 (-1)
+        1553684: 4 (0)
+        12683156: 6 (2)
+        11100544: 4 (-2)
+        12249484: 4 (0)
+        7753432: 2 (-2)
+
+        >>> for sequence in SecretNumber(123).sequences(6):
+        ...     print(sequence)
+        Sequence(sequence=(-3, 6, -1, -1), price=4)
+        Sequence(sequence=(6, -1, -1, 0), price=4)
+        Sequence(sequence=(-1, -1, 0, 2), price=6)
+        """
+        diffs = []
+        for secret_number in self.simulate(iterations):
+            diffs.append(secret_number.price_diff())
+            if len(diffs) == 4:
+                yield Sequence(tuple(diffs), secret_number.price())
+                diffs.pop(0)
 
     def simulate(self, iterations):
         previous = self
@@ -76,11 +124,24 @@ class SecretNumber:
         """
         return self.number % 10
 
+    def price_diff(self):
+        """
+        >>> SecretNumber(10, SecretNumber(5)).price_diff()
+        -5
+        """
+        if self.previous_secret_number is None:
+            raise ValueError("No previous number to diff against.")
+        else:
+            return self.price() - self.previous_secret_number.price()
+
     def as_integer(self):
         return self.number
 
     def __repr__(self):
-        return repr(self.number)
+        return f"{self.number}: {self.price()} ({self.price_diff()})"
+
+class Sequence(collections.namedtuple("Sequence", ["sequence", "price"])):
+    pass
 
 if __name__ == "__main__":
     import doctest
