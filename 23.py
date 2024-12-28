0b7bc54c1f836d7ac70be88d00ddd9b68e86c8c0
Rickard Lindberg <rickard@rickardlindberg.me>
2024-12-28 22:11:16 +0100

    Day 23, part 1.

--- 23.py
+++ 23.py
@@ -1,5 +1,6 @@
 """
->>> NetworkMapParser().parse()
+>>> NetworkMapParser().parse().find_3connections()
+1284
 """
 
 example = """\
@@ -37,6 +38,7 @@ tb-vc
 td-yn
 """
 
+import itertools
 import subprocess
 
 class NetworkMapParser:
@@ -56,6 +58,7 @@ class NetworkMap:
 
     def __init__(self):
         self.connections = {}
+        self.computers = set()
 
     def add(self, a, b):
         if a not in self.connections:
@@ -66,6 +69,30 @@ class NetworkMap:
             self.connections[b] = [a]
         else:
             self.connections[b].append(a)
+        self.computers.add(a)
+        self.computers.add(b)
+
+    def are_connected(self, a, b):
+        return b in self.connections.get(a, [])
+
+    def find_3connections(self):
+        count = 0
+        for computers in self.all_sets_of_three():
+            if computers.all_connected(self) and computers.one_with_t():
+                count += 1
+        return count
+
+    def all_sets_of_three(self):
+        """
+        >>> network_map = NetworkMap()
+        >>> network_map.add("a", "b")
+        >>> network_map.add("b", "c")
+        >>> network_map.add("c", "d")
+        >>> list(network_map.all_sets_of_three())
+        [Computers(a, b, c), Computers(a, b, d), Computers(a, c, d), Computers(b, c, d)]
+        """
+        for names in itertools.combinations(self.computers, 3):
+            yield Computers(names)
 
     def visualize(self):
         with open("23.dot", "w") as f:
@@ -81,6 +108,26 @@ class NetworkMap:
                 parts.append(f"{a} -> {item};")
         parts.append("}")
         return "\n".join(parts)
+
+class Computers:
+
+    def __init__(self, names):
+        self.names = names
+
+    def all_connected(self, network):
+        return all(
+            network.are_connected(a, b)
+            for (a, b) in itertools.combinations(self.names, 2)
+        )
+
+    def one_with_t(self):
+        for name in self.names:
+            if name.startswith("t"):
+                return True
+        return False
+
+    def __repr__(self):
+        return f"Computers({', '.join(self.names)})"
 
 if __name__ == "__main__":
     import sys
