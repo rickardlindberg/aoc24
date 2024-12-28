29b6728508759c8b3af60fcdd24714427ab67e31
Rickard Lindberg <rickard@rickardlindberg.me>
2024-12-28 19:56:17 +0100

    A little clean up.

--- 22.py
+++ 22.py
@@ -29,33 +29,39 @@ class SecretNumbers:
 
     def sum_2000(self):
         return sum(
-            secret_number.simulate_2000()
+            secret_number.simulate_2000().as_integer()
             for secret_number in self.secret_numbers
         )
 
 class SecretNumber:
 
-    def __init__(self, secret_number):
-        self.secret_number = secret_number
+    def __init__(self, number):
+        self.number = number
 
     def simulate_2000(self):
         """
         >>> SecretNumber(1).simulate_2000()
         8685429
         """
-        secret_number = self.secret_number
+        number = self.number
         for i in range(2000):
-            secret_number = self.mix_and_prune(secret_number, secret_number*64)
-            secret_number = self.mix_and_prune(secret_number, secret_number//32)
-            secret_number = self.mix_and_prune(secret_number, secret_number*2048)
-        return secret_number
+            number = self.mix_and_prune(number, number*64)
+            number = self.mix_and_prune(number, number//32)
+            number = self.mix_and_prune(number, number*2048)
+        return SecretNumber(number)
 
     def mix_and_prune(self, a, b):
-        secret_number = operator.xor(a, b)
-        if secret_number == 100000000:
+        number = operator.xor(a, b)
+        if number == 100000000:
             return 16113920
         else:
-            return secret_number % 16777216
+            return number % 16777216
+
+    def as_integer(self):
+        return self.number
+
+    def __repr__(self):
+        return repr(self.number)
 
 if __name__ == "__main__":
     import doctest
